1f5784a17027cc4262f8087bdf32d7fe64ccac95
Sergii Ternytskyi <serhij86@gmail.com>
2023-02-21 00:13:42 +0200

    Update getAll.js

--- controlers/contacts/getAll.js
+++ controlers/contacts/getAll.js
@@ -1,7 +1,13 @@
+const { NotFound } = require("http-errors");
+
 const { getAllContacts } = require("../../service");
 
 const getAll = async (req, res) => {
     const result = await getAllContacts();
+
+    if (!result) {
+        throw NotFound("Not found");
+    }
 
     res.status(200).json({
         status: 200,
